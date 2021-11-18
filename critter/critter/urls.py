8e3c78735f71dfc3dd2a639a150fd820155e353a
Rosario Jauregui <rjjauregui@MacBook-Pro.local>
2021-11-18 09:08:48 -0800

    cities in drop down menue done - submit cities form next

--- critter/critter/urls.py
+++ critter/critter/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('get_weather_info.urls')),
-    # path('', include('main_page.urls'))
+    # path('',include('get_weather_info.urls')),
+    path('', include('main_page.urls'))
 ]
--- critter/get_weather_info/views.py
+++ critter/get_weather_info/views.py
@@ -4,12 +4,16 @@ from django.shortcuts import render
 # Create your views here.
 
 def weather_index(request):
-    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=e9e08908675df4cc8b0d9478f5e607dc'
-    #'api.openweathermap.org/data/2.5/weather?q={}&appid=e9e08908675df4cc8b0d9478f5e607dc'
+    url = 'http://api.openweathermap.org/data/2.5/weather?lat={}&lon={}&units=imperial&appid=e9e08908675df4cc8b0d9478f5e607dc'
+    # 'http://api.openweathermap.org/data/2.5/weather?q={}&units=imperial&appid=e9e08908675df4cc8b0d9478f5e607dc'
 
-    city = 'Santa Cruz'    
+    city = 'Santa Cruz california'    
+    lat = '37'
+    lon = '122'
 
-    r = requests.get(url.format(city))
+    r = requests.get(url.format(lat,lon))
+
+    # r = requests.get(url.format(city))
 
     print(r.text)
 
