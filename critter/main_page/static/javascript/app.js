ae77116e323c90c5dd2ed9e41f060c1db172b397
rjjauregui15 <rjjauregui15@gmail.com>
2021-11-21 16:11:21 -0800

    got the talk between the 2 computers

--- critter/main_page/static/javascript/app.js
+++ critter/main_page/static/javascript/app.js
@@ -1,5 +1,5 @@
 //Selectors
-const locationPicked = document.getElementById('locations')
+const locationPicked = document.getElementById('locations');
 const todoButton = document.querySelector('.todo-button');
 
 //Event Listeners
@@ -9,6 +9,31 @@ todoButton.addEventListener('click', addCombo);
 //Functions 
 function addCombo(event) {
     // Prevent form from submitting
-    event.preventDefault();
-    console.log(locationPicked.value);
+  event.preventDefault();
+  // console.log('mom')
+  //   console.log('dad')
+    // console.log('hi mom')
+    // console.log(locationPicked.value);
+
+    let request = new XMLHttpRequest();
+    const url = 'http://127.0.0.1:8000/test/'
+    request.open('GET', url);
+    request.responseType = 'text';
+    request.onload = function() {
+
+      console.log(request.responseText);
+    
+      };
+    request.send();
+
+    // const Http = new XMLHttpRequest();
+    // Http.open("GET", url);
+    // Http.send();
+
+    // Http.onreadystatechange = (e) => {
+    // console.log(Http.responseText)
+    // }
+
+
 }
+
--- critter/main_page/views.py
+++ critter/main_page/views.py
@@ -1,7 +1,20 @@
+from django.http import HttpResponse
+from django.http.response import JsonResponse
 import requests
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return render(request, 'index.html') 
+    return render(request, 'index.html')
+ 
+def test(request):
+    # response = HttpResponse({'text is the beach in this bitch'})
+    # return response
+
+    # print(a)
+    # return HttpResponse(index.html)
+    # context = {'list': 'cold'}
+    # return render(request, 'index.html', context)
+    return JsonResponse({'text': 'hello moiz'})
+    
