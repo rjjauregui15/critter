7384b957ca78afea861b2c56dbf656ba108d215e
rjjauregui15 <rjjauregui15@gmail.com>
2021-11-29 09:33:07 -0800

    starting on the brains of the page

--- critter/main_page/static/javascript/app.js
+++ critter/main_page/static/javascript/app.js
@@ -1,39 +1,48 @@
 //Selectors
-const locationPicked = document.getElementById('locations');
-const todoButton = document.querySelector('.todo-button');
+const searchButton = document.querySelector('.search_button');
+const checkMark = document.querySelector('#check_mark_box');
 
 //Event Listeners
-todoButton.addEventListener('click', addCombo);
-
+searchButton.addEventListener('click', checkLocation);
 
 //Functions 
-function addCombo(event) {
-    // Prevent form from submitting
+function checkLocation(event) {
+  // Prevent form from submitting
   event.preventDefault();
-  // console.log('mom')
-  //   console.log('dad')
-    // console.log('hi mom')
-    // console.log(locationPicked.value);
 
-    let request = new XMLHttpRequest();
-    const url = 'http://127.0.0.1:8000/test/'
-    request.open('GET', url);
-    request.responseType = 'text';
-    request.onload = function() {
+  //console.log(checkMark.checked);
 
-      console.log(request.responseText);
+  
+
+  // if checkMArk.checked == 1 
+  // call function to get cordinites 
+  // get cordinates  and call other fintion to ship out and ship them out
+  // have function to recives message then another to print out to screen
+
+  // if check if long anf lat have valid inputs
+  // get cordinates and call other funtion to ship them out 
+  // have function to recives message then another to print out to screen
+
+  // else, ask user for input 
+
+
+  // function list
+  - get user Input
+  - faction to send the information Over
+  - function to recive information
+  - funtion to put ont he screen 
+
+
+    // let request = new XMLHttpRequest();
+    // const url = 'http://127.0.0.1:8000/test/'
+    // request.open('GET', url);
+    // request.responseType = 'text';
+    // request.onload = function() {
+
+    //   console.log(request.responseText);
     
-      };
-    request.send();
-
-    // const Http = new XMLHttpRequest();
-    // Http.open("GET", url);
-    // Http.send();
-
-    // Http.onreadystatechange = (e) => {
-    // console.log(Http.responseText)
-    // }
-
+    //   };
+    // request.send();
 
 }
 
